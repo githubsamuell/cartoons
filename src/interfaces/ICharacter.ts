2d9a72c980a8f9398cf9461383b5a04273976009
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2022-05-26 02:36:19 -0300

    create ICharacterInterface

---
+++ src/interfaces/ICharacter.ts
@@ -0,0 +1,24 @@
+export interface ICharacter {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+  origin: OriginCharacter;
+  location: LocationCharacter;
+}
+
+interface OriginCharacter {
+  name: string;
+  url: string;
+}
+
+interface LocationCharacter {
+  name: string;
+  url: string;
+}
